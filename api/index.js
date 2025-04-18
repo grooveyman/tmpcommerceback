b2db48f984016d024aca27af28d0b5ff0ee86994
grooveyman <edegraftamaning85@mail.com>
2025-04-18 09:15:38 +0000

    listener

--- api/index.js
+++ api/index.js
@@ -54,9 +54,9 @@ connectDb().then(() => {
 //   console.error("Database connection failed:", err.message);
 });
 
-// app.listen(5001, () => {
-//   console.log("Server is running on port 5001");
-// });
+app.listen(5002, () => {
+  console.log("Server is running on port 5002");
+});
 
 
 // const handler = async (req, res) => {
