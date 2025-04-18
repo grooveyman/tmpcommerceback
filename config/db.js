1db8d13a927f03eed700cc8e1fea59446cb7ec83
grooveyman <edegraftamaning85@mail.com>
2025-04-18 08:32:10 +0000

    vercel donfig

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const dbConnect = async() => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
         isConnected = true;
-        console.log(`Successfully connected to mongodb`);
+        // console.log(`Successfully connected to mongodb`);
     }catch(error){
         console.log(`Error: ${error.message}`);
         process.exit(1);
